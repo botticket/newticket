c4c9d3787e6d63b36df03b0845a586e57502140a
botticket <59758461+botticket@users.noreply.github.com>
2020-07-06 08:06:07 +0700

    Add files via upload

--- flex_stock.py
+++ flex_stock.py
@@ -1807,7 +1807,7 @@ def flex_stock(text,price_now,change,chgp,notice,start,buy,stop,target,avg):
                 "contents": [
                 {
                     "type": "text",
-                    "text": "{} ~ {}".format(start,buy),
+                    "text": "{} + 2 ช่อง".format(start),
                     "flex": 3,
                     "size": "lg"
                 }

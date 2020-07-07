0c72d832ad7be761e69770e81c8d78b9e251c9fa
botticket <59758461+botticket@users.noreply.github.com>
2020-07-08 00:20:10 +0700

    Add files via upload

--- ticketapp.py
+++ ticketapp.py
@@ -75,16 +75,16 @@ def handle_message(event):
     print(request_text)
     linechat(request_text)
 
-    # result_from_dialogflow = detect_intent_texts(project_id="worldstock-iardyn",
-    #                                     session_id=userid ,
-    #                                     text=text_from_user , 
-    #                                     language_code="th")
+    result_from_dialogflow = detect_intent_texts(project_id="worldstock-iardyn",
+                                        session_id=userid ,
+                                        text=text_from_user , 
+                                        language_code="th")
     
-    # action = result_from_dialogflow["action"]
-    # response = result_from_dialogflow["fulfillment_messages"] #as list
+    action = result_from_dialogflow["action"]
+    response = result_from_dialogflow["fulfillment_messages"] #as list
 
-    # print("action : " + action)
-    # print("response : " + str(response))
+    print("action : " + action)
+    print("response : " + str(response))
 
     try:
         if 'Hello Bot' in text_from_user:    
@@ -638,142 +638,6 @@ def handle_message(event):
                 reply_msg(reply_token,data=flex_to_reply,bot_access_key=channel_access_token)
                 return 'OK'
             setcheck()
-
-        elif action == "crypto_response":
-            from urllib.request import Request, urlopen
-            from bs4 import BeautifulSoup as soup 
-            from pandas_datareader import data
-            from datetime import datetime
-            
-            text_from_user = text_from_user.upper()
-            code = [text_from_user]
-            codes = list(map(lambda e: e + '-USD', code))
-
-            class crypto:
-                def __init__(self,code):
-                    self.code = code
-                def ticket(self):
-                    end = datetime.now()
-                    start = datetime(end.year,end.month,end.day)
-                    list = self.code
-
-                    dfY = data.DataReader(f'{list}', data_source="yahoo", start=yearly, end=end)
-                    dfM = data.DataReader(f'{list}', data_source="yahoo", start=monthly, end=end)
-                    #2020-01-01 = Y M D
-
-                    OpenY = dfY['Open'].iloc[1]
-                    OpenY  = '%.2f'%OpenY
-                    OpenY = str(OpenY)
-
-                    OpenM = dfM['Open'].iloc[1]
-                    OpenM  = '%.2f'%OpenM
-                    OpenM = str(OpenM)
-
-                    OpenD = dfY['Open'].iloc[-1]
-                    OpenD  = '%.2f'%OpenD
-                    OpenD = str(OpenD)
-
-                    Close = dfY['Close'].iloc[-1]
-                    Close  = '%.2f'%Close
-                    Close = str(Close)
-
-                    Prev = dfY['Close'].iloc[-2]
-                    Prev  = '%.2f'%Prev
-                    Prev = str(Prev)
-                    
-                    barY = ((float(Close) - float(OpenY)) / float(OpenY) )*100
-                    barY = '%.2f'%barY
-                    barY = float(barY)
-
-                    barM = ((float(Close) - float(OpenM)) / float(OpenM) )*100
-                    barM = '%.2f'%barM
-                    barM = float(barM)
-
-                    LongY = float(OpenM) * 1.01
-                    LongY = '%.2f'%LongY
-                    LongY = str(LongY) 
-
-                    stop_longY = float(OpenM) * 0.985
-                    stop_longY = '%.2f'%stop_longY
-                    stop_longY = str(stop_longY)
-
-                    exit_long1 = float(OpenD) * 1.05
-                    exit_long1 = '%.2f'%exit_long1
-                    exit_long1 = str(exit_long1)
-
-                    exit_long2 = float(OpenD) * 1.10
-                    exit_long2 = '%.2f'%exit_long2
-                    exit_long2 = str(exit_long2)
-
-                    exit_long3 = float(OpenD) * 1.20
-                    exit_long3 = '%.2f'%exit_long3
-                    exit_long3 = str(exit_long3)
-
-                    shortY = float(OpenM) * 0.985
-                    shortY = '%.2f'%shortY
-                    shortY = str(shortY) 
-
-                    stop_shortY = float(OpenM) * 1.01
-                    stop_shortY = '%.2f'%stop_shortY
-                    stop_shortY = str(stop_shortY)
-
-                    exit_short1 = float(OpenD) * 0.95
-                    exit_short1 = '%.2f'%exit_short1
-                    exit_short1 = str(exit_short1)
-
-                    exit_short2 = float(OpenD) * 0.90
-                    exit_short2 = '%.2f'%exit_short2
-                    exit_short2 = str(exit_short2)
-
-                    exit_short3 = float(OpenD) * 0.80
-                    exit_short3 = '%.2f'%exit_short3
-                    exit_short3 = str(exit_short3)
-                
-                    change = float(Close) - float(Prev)
-                    change = '%.2f'%change
-                    change = str(change) 
-
-                    chgp = (float(change) / float(Prev))*100
-                    chgp = '%.2f'%chgp
-                    chgp = str(chgp) 		
-                    
-                    text1 = exit_long1 + ' | ' + exit_long2 + ' | ' + exit_long3 
-                    text2 = exit_short1 + ' | ' + exit_short2 + ' | ' + exit_short3 
-
-                    alert1 = 'Long'
-                    alert2 = 'Short'
-
-                    text = code
-                    price_now = float(Close) 
-                    change = str(change) 
-
-                    if barM >= 0:
-                        notice = alert1
-                        start = OpenM
-                        buy = LongY
-                        stop = stop_longY
-                        target = text1
-                        number = '1'
-                    else:
-                        notice = alert2
-                        start = OpenM
-                        buy = shortY
-                        stop = stop_shortY
-                        target = text2 
-                        number = '2'
-
-                    word_to_reply = '{}'.format(text) + '\n' + 'now {} {} ({}%)'.format(price_now,change,chgp)
-                    result = 'Position: {}'.format(notice) + '\n' + 'Range: {} - {} '.format(start,buy) + '\n' + 'Stop: {}'.format(stop) + '\n' + 'Target: {}'.format(target)
-                    linechat(word_to_reply)
-
-                    bubble = flex_crypto(text,price_now,change,chgp,notice,start,buy,stop,target)
-                    
-                    flex_to_reply = SetMessage_Object(bubble)
-                    reply_msg(reply_token,data=flex_to_reply,bot_access_key=channel_access_token)
-                    return 'OK'
-
-            for code in codes:
-                crypto(code).ticket()
 
         else:
             from bs4 import BeautifulSoup as soup
